90b34b9e050f35dcf23ee01d1403b2a5fa7829df
mhelpineda <pinedamhel14@gmail.com>
2017-09-01 16:32:49 +0800

    status2

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function createCORSRequest(method, url) {
     // Check if the XMLHttpRequest object has a "withCredentials" property.
     // "withCredentials" only exists on XMLHTTPRequest2 objects.
     xhr.open(method, url, true);
-    console.log('1 get return status');
+    console.log('1 get return status2');
 
   } else if (typeof XDomainRequest != "undefined") {
 
@@ -37,7 +37,7 @@ function createCORSRequest(method, url) {
     console.log('3');
 
   }
-  var ss = JSON.parse(xhr.responseText);
+  
 
  		console.log(xhr.status);
   return xhr;
