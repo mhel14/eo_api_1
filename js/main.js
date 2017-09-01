be5a74670a8c982fffe1aa9a763747b74f8166de
mhelpineda <pinedamhel14@gmail.com>
2017-09-01 16:05:58 +0800

    logs

--- js/main.js
+++ js/main.js
@@ -21,17 +21,20 @@ function createCORSRequest(method, url) {
     // Check if the XMLHttpRequest object has a "withCredentials" property.
     // "withCredentials" only exists on XMLHTTPRequest2 objects.
     xhr.open(method, url, true);
+    console.log('1');
+
   } else if (typeof XDomainRequest != "undefined") {
 
     // Otherwise, check if XDomainRequest.
     // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
     xhr = new XDomainRequest();
     xhr.open(method, url);
-
+    console.log('2');
   } else {
 
     // Otherwise, CORS is not supported by the browser.
     xhr = null;
+    console.log('3');
 
   }
   return xhr;
@@ -40,6 +43,7 @@ function createCORSRequest(method, url) {
 var xhr = createCORSRequest('GET', url);
 if (!xhr) {
   throw new Error('CORS not supported');
+  console.log('4');
 }
 
 $('.get-data').click(function() {
